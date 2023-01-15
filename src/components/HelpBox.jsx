7f9b92935f6891ebe99ab872d4e81cd8d75ed99a
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-15 18:11:12 +0530

    changed h3 to h2 in Helpbox.jsx

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h3>{title}</h3>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );

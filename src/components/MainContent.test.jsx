5f7eeb355991e5f1fc18d81cc4e791eb5f27a909
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-15 19:53:23 +0530

    Added a comment. [skip actions]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+// My tests!
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
